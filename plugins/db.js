67354a8a1b9c8ae4cebd1a0e1c4be277ae5b5fb3
unknown <1029063696@qq.com>
2020-04-15 23:03:35 +0800

    mongodb adress change

--- plugins/db.js
+++ plugins/db.js
@@ -1,6 +1,6 @@
 module.exports =app =>{
     var mongoose=require('mongoose');
-    mongoose.connect('mongodb://localhost:27017/miyou',{
+    mongoose.connect('mongodb://127.0.0.1:27017/miyou',{
         useNewUrlParser:true,
         useUnifiedTopology: true
     });
